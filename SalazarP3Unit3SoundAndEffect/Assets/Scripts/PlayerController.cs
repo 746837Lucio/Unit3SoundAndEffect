f78bbeea84a97d0f880ac789534e25e959df8843
746837Lucio <luciosalazar0503@gmail.com>
2024-04-18 11:37:43 -0700

    fisn ihed the ganme
    i adeed a cpouipole more lines tro my c ode and i added a douible jump

--- SalazarP3Unit3SoundAndEffect/Assets/Scripts/PlayerController.cs
+++ SalazarP3Unit3SoundAndEffect/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
     private Rigidbody playerRb;
     private Animator playerAnim;
     private AudioSource playerAudio;
@@ -15,7 +16,9 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
-    
+    public bool canDoubleJump = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,14 +31,29 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {//single jump if
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
             playerAudio.PlayOneShot(jumpSound, 1.0f);
+            canDoubleJump = true;
+
+
+        }
+
+        //double jump if
+        if (Input.GetKeyDown(KeyCode.Space) && playerRb.velocity.y > 0f && canDoubleJump && !gameOver)
+        {
+            canDoubleJump = false;
+
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+
+            playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 1);
         }
 
     }
